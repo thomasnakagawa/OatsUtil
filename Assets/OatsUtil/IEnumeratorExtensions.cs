c61d332fb2eb6c51d5debec4144aa49646461d0e
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-02-08 22:45:53 -0800

    Add IEnumerator extensions

---
+++ Assets/OatsUtil/IEnumeratorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace OatsUtil
+{
+    public static class IEnumeratorExtensions
+    {
+        /// <summary>
+        /// Runs an IEnumerator after another IEnumerator in a sequence when executed in a coroutine
+        /// </summary>
+        /// <param name="enumerator">The Enumerator to run first</param>
+        /// <param name="next">The Enumerator to run next</param>
+        /// <returns>Enumerator with the Enumerators concatenated</returns>
+        public static IEnumerator Then(this IEnumerator enumerator, IEnumerator next)
+        {
+            yield return enumerator;
+            yield return next;
+        }
+
+        /// <summary>
+        /// Runs an Action after an IEnumerator in a sequence when executed in a coroutine
+        /// </summary>
+        /// <param name="enumerator">The Enumerator to run first</param>
+        /// <param name="next">The action to run next</param>
+        /// <returns>Enumerator with the action invoked after the enumerator</returns>
+        public static IEnumerator Then(this IEnumerator enumerator, System.Action next)
+        {
+            yield return enumerator;
+            next.Invoke();
+        }
+    }
+}
